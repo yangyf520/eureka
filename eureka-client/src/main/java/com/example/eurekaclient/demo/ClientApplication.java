39ff0dc98d3dd9bef97aa2c3a50c0d90b5d56df7
Yangyf <yyang@cccis.com>
2020-04-04 15:37:53 +0800

    解决Unable to connect to Command Metric Stream

--- eureka-client/src/main/java/com/example/eurekaclient/demo/ClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/demo/ClientApplication.java
@@ -3,11 +3,15 @@ package com.example.eurekaclient.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * EurekaClient启动类
  */
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ClientApplication {
 
--- eureka-client/src/main/java/com/example/eurekaclient/demo/HelloController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.eurekaclient.demo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +15,14 @@ public class HelloController {
     @Value("${server.port}")
     String port;
 
+    @HystrixCommand(fallbackMethod = "fallbackError")
     @RequestMapping("/hi")
     public String home(@RequestParam String name) {
+        int i = 1/0;
         return "welcome " + name + ",your port:" + port;
+    }
+
+    public String fallbackError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
